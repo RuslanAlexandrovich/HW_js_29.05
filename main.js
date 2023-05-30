426d7ad8c71262c202c32d711403369ff9ea387c
Ruslan Alexandrovich <Motuzkinra@gmail.com>
2023-05-31 01:50:59 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// let nameUs = prompt("Введите имя:");
+// alert("Hello " + nameUs)
+
+// let birthday = +prompt("Укажите год рождения:");
+// const year = 2023;
+// alert("Вам " + (year - birthday))
+
+// let side = +prompt("Укажите одну из сторон квадрата:");
+// alert("Площадь равна " + side**2 + " М2")
+
+// let r = +prompt("Введите радиус:");
+// alert("Площадь круга равна: " + (Math.PI * r**2))
+
+// let way = +prompt("Введите расстояние км:");
+// let hours = +prompt("Введите желаемое время в пути:");
+// alert("Рекомендуемая скорость км.ч: " + way / hours)
+
+// let dollar = +prompt("Введите сумму $");
+// const euro = 0.94;
+// alert(dollar / euro + " EURO")
+
+// let gb = +prompt("Укажите обьем накопителя Gb:");
+// alert(Math.trunc(gb * 1024 / 820) + " файлов поместится")
+
+// let sum = +prompt("Введите сумму денег грн:");
+// let cena = +prompt("Введите стоимость одной единицы товара:");
+// alert("Можете купить " + Math.trunc(sum / cena) + " шт Ваша сдача: " + (sum % cena))
+
+// var num = +prompt("Введите трёхзначное число:");
+// var a = Math.trunc(num / 10);
+// var b = String(num % 10);
+// var c = Math.trunc(a / 10);
+// var d = a % 10;
+// var e = Math.trunc(c / 10);
+// var f = c % 10;
+
+// alert(b + d + f);
+
+// var x = +prompt("Введите трёхзначное число:");
+// var y = 0;
+// for(; x; x = Math.floor(x / 10)) {
+//     y *= 10;
+//     y += x % 10;
+// }
+// alert(y);
+
+// let num = +prompt("Введите целое число:");
+// console.log(num % 2 == 0);
+// alert(num % 2 == 0)
+
---
+++ main_2.js
@@ -0,0 +1,52 @@
+// let num = +prompt("Введите число:");
+// alert("В квадрате: " + num**2)
+
+// let num1 = +prompt("Введите число 1:");
+// let num2 = +prompt("Введите число 2:");
+// alert("Среднее арифметическое: " + (num1 + num2) / 2)
+
+// let num = +prompt("Введите динну стороны квадрата:");
+// alert("Площадь равна: " + num**2)
+
+// let num = +prompt("Введите расстояние в км:");
+// const a = 0.62;
+// alert(num + " км = " + num * a + " m")
+
+// let num = +prompt("Введите первое число:");
+// let num2 = +prompt("Введите второе число:");
+// alert("сумма = " + (num + num2) + " разница = " + (num - num2) + " умножение = " + num * num2)
+
+// let a = +prompt("a * x + b = 0; Введите (а)");
+// let b = +prompt("a * x + b = 0; Введите (b)");
+// if (a != 0){
+// let x = -b / a;
+// alert("x = " + x)
+// } else {
+//     alert("Недупустимое значение (а)")
+// }
+
+
+// let a = +prompt("a * x + b = 0; Введите (a)")
+// let b = +prompt("a * x + b = 0; Введите (b)");
+// if (String(a) === "NaN" || a === 0 || String(b) === "NaN"){
+//     alert("Недопустимое значение (а)")
+// } else {
+//     console.log(a);
+//     let x = -b / a;
+//     alert("x =  " + x)
+// }
+
+// let hour = +prompt("Введите время (часы)") * 60;
+// let minut = +prompt("Введите время (минуты)");
+// let minutes = 60 - minut;
+// alert("До завтра осталось:  " + parseInt((1440 - (hour + minut)) / 60) + " часов " + minutes + " минут")
+
+// let a = prompt("Введите трёхзначное число:");
+// alert("второе число:  " + a[1])
+
+// let a = prompt("Введите пятизначное число:");
+// alert("Результат:  " + a[4]+a[0]+a[1]+a[2]+a[3] )
+
+// let num = +prompt("Сумма выручки за месяц: ");
+// num = num * 0.1 + 250;
+// alert("Ваша ЗП  " + num)
